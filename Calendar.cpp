986c5d0595f3abb95c8f65e3967bd0da079ed3e6
Koff Daniil <119159791+II-KoDa-II@users.noreply.github.com>
2022-12-13 11:17:58 +0700

    Add files via upload

--- Calendar.cpp
+++ Calendar.cpp
@@ -10,27 +10,24 @@
 
 using namespace std;
 
-int calendarDisplay (int weekPosition, int days, string month) {
-  int index;
+int daysPerWeek = 7;
+
+int calendarDisplay (int weekPosition, int dayIndexs, string month) {
+  int dayIndex;
   
   cout << "\n\n\n\n" << month << "----------\n\nMon Tue Wed Thu Fri Sat Sun\n";
   
-  for (index = 1; index < weekPosition; ++index) {
+  for (dayIndex = 1; dayIndex < weekPosition; ++dayIndex) {
     cout << "    ";
   }
   
-  for (index = 1; index <= days; ++index) {
-    cout << " " << index << " ";
-    
-    if (index < 10) {
-      cout << " ";
-    }
+  for (dayIndex = 1; dayIndex <= dayIndexs; ++dayIndex) {
+    printf("%02d  ", dayIndex);
     
     ++weekPosition;
-    
-    if (weekPosition > 7) {
+    if (weekPosition > daysPerWeek) {
       cout << "\n";
-      weekPosition -= 7;
+      weekPosition -= daysPerWeek;
     }
   }
   
@@ -51,27 +48,22 @@ int main() {
   if (startYear <= 0) {
     --leap;
   }
-  
-  leap += (startYear - 1) / 4;
+  leap += (startYear - 1) / 4 - (startYear - 1) / 100 + (startYear - 1) / 400;
   
   weekPosition += startYear + leap;
-  
-  while (weekPosition > 7) {
-    weekPosition -= 7;
+  while (weekPosition > daysPerWeek) {
+    weekPosition -= daysPerWeek;
   }
-  
   while (weekPosition < 1) {
-    weekPosition += 7;
+    weekPosition += daysPerWeek;
   }
   
   weekPosition = calendarDisplay(weekPosition, 31, "January");
-  
-  if (startYear % 4 == 0) {
+  if ((startYear % 4 == 0 && startYear % 100 != 0) || startYear % 400 == 0) {
     weekPosition = calendarDisplay(weekPosition, 29, "February");
   } else {
     weekPosition = calendarDisplay(weekPosition, 28, "February");
   }
-  
   weekPosition = calendarDisplay(weekPosition, 31, "March");
   weekPosition = calendarDisplay(weekPosition, 30, "April");
   weekPosition = calendarDisplay(weekPosition, 31, "May");
